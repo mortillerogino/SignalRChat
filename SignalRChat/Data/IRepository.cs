b8e9be713b2e40b2de058cc622af3aefb5918b4a
Gino Mortillero <mortillerogino@gmail.com>
2019-09-20 13:15:09 +0800

    Added Generic Repository

---
+++ SignalRChat/Data/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Data
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAsync(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+        IQueryable<TEntity> Query(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+        Task<TEntity> GetByIdAsync(object id);
+
+        Task<TEntity> GetFirstOrDefaultAsync(
+        Expression<Func<TEntity, bool>> filter = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+        Task InsertAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        Task DeleteAsync(object id);
+
+        int GetCount();
+    }
+}
---
+++ SignalRChat/Data/Repository.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual async Task<List<TEntity>> GetAsync(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            foreach (Expression<Func<TEntity, object>> include in includes)
+                query = query.Include(include);
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return await query.ToListAsync().ConfigureAwait(false);
+        }
+
+        public virtual IQueryable<TEntity> Query(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            foreach (Expression<Func<TEntity, object>> include in includes)
+                query = query.Include(include);
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return query;
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public virtual async Task<TEntity> GetFirstOrDefaultAsync(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            foreach (Expression<Func<TEntity, object>> include in includes)
+                query = query.Include(include);
+
+            return await query.FirstOrDefaultAsync(filter).ConfigureAwait(false);
+        }
+
+        public virtual async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity).ConfigureAwait(false);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual async Task DeleteAsync(object id)
+        {
+            TEntity entityToDelete = await _dbSet.FindAsync(id).ConfigureAwait(false);
+            if (_context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entityToDelete);
+            }
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public int GetCount()
+        {
+            return _dbSet.Count();
+        }
+
+
+    }
+}
