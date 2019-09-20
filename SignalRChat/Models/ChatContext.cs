9dc1feae0aaf0fea4fb5ecf8ff5ad7bcae74bb02
Gino Mortillero <mortillerogino@gmail.com>
2019-09-20 13:22:20 +0800

    Added Context, Configured SQL Server With Connection String

---
+++ SignalRChat/Models/ChatContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Models
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+
+    }
+}
--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SignalRChat.Areas.Chat.Models;
+using SignalRChat.Models;
 
 namespace SignalRChat
 {
@@ -25,6 +27,12 @@ namespace SignalRChat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<ChatContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ChatDb"));
+            });
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
