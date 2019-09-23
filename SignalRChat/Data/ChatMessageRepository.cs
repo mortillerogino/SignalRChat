e9ee4d881701a9a41f92218c2c35f1e175b969ee
Gino Mortillero <mortillerogino@gmail.com>
2019-09-23 09:59:02 +0800

    Added ChatMessage Repository

---
+++ SignalRChat/Data/ChatMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRChat.Areas.Chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Data
+{
+    public class ChatMessageRepository : Repository<ChatMessage>, IChatMessageRepository
+    {
+        public ChatMessageRepository(DbContext dbContext) 
+            : base (dbContext)
+        {
+
+        }
+    }
+}
---
+++ SignalRChat/Data/IChatMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using SignalRChat.Areas.Chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Data
+{
+    public interface IChatMessageRepository : IRepository<ChatMessage>
+    {
+    }
+}
