d9f45bc36e3e57d12d610441955dc331f7bdd0de
Gino Mortillero <mortillerogino@gmail.com>
2019-09-23 09:56:54 +0800

    Added Chatroom Repository

---
+++ SignalRChat/Data/ChatroomRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRChat.Areas.Chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Data
+{
+    public class ChatroomRepository : Repository<Chatroom>, IChatroomRepository
+    {
+        public ChatroomRepository(DbContext dbContext) 
+            : base(dbContext)
+        {
+
+        }
+    }
+}
