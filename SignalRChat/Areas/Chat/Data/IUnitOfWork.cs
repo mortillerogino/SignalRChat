97ee7e5a30a04c5385ce9f22f40ac843f9c8cd65
Gino Mortillero <mortillerogino@gmail.com>
2019-09-23 10:10:58 +0800

    Added UnitOfWork entity

---
+++ SignalRChat/Areas/Chat/Data/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using SignalRChat.Areas.Chat.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Areas.Chat.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IChatroomRepository ChatroomRepository { get; }
+        IChatMessageRepository ChatMessageRepository { get; }
+
+        void Commit();
+
+        Task<int> CommitAsync();
+    }
+}
---
+++ SignalRChat/Areas/Chat/Data/UnitOfWork.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Areas.Chat.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        private IChatroomRepository _chatroomRepository;
+        public IChatroomRepository ChatroomRepository
+        {
+            get
+            {
+                if (_chatroomRepository == null)
+                {
+                    _chatroomRepository = new ChatroomRepository(_dbContext);
+                }
+
+                return _chatroomRepository;
+            }
+        }
+
+        private IChatMessageRepository _chatMessageRepository;
+        public IChatMessageRepository ChatMessageRepository
+        {
+            get
+            {
+                if (_chatMessageRepository == null)
+                {
+                    _chatMessageRepository = new ChatMessageRepository(_dbContext);
+                }
+
+                return _chatMessageRepository;
+            }
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        private bool _disposedValue = false; 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
